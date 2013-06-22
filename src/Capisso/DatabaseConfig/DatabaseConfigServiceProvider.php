3a8f85f5f391e2f2004f27e8cc7e6a0ee12ceb69
clone1018 <luke@axxim.net>
2013-06-22 10:20:28 -0500

    Initial Commit

---
+++ src/Capisso/DatabaseConfig/DatabaseConfigServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Capisso\DatabaseConfig;
+
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseConfigServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('capisso/database-config');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
